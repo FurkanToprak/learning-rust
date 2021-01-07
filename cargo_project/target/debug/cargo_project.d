7c1ab0181b6fd200f3fb1444b8aad91994b10c03
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-01-07 16:42:44 -0600

    Removed build.

--- cargo_project/target/debug/cargo_project.d
+++
@@ -1,1 +0,0 @@
-/home/furgo/Desktop/learning_rust/cargo_project/target/debug/cargo_project: /home/furgo/Desktop/learning_rust/cargo_project/src/main.rs
--- cargo_project/target/debug/deps/cargo_project-89ba86924c0d3b8b.d
+++
@@ -1,5 +0,0 @@
-/home/furgo/Desktop/learning_rust/cargo_project/target/debug/deps/cargo_project-89ba86924c0d3b8b: src/main.rs
-
-/home/furgo/Desktop/learning_rust/cargo_project/target/debug/deps/cargo_project-89ba86924c0d3b8b.d: src/main.rs
-
-src/main.rs:
